165869da533b763d4eee69478a1c5d27976b3b4d
Anmolgit1234 <anmolakolkar67@gmail.com>
2022-09-14 14:24:38 +0530

    ny first commit

---
+++ src/test/java/VentueCapitalSystem/Login.java
@@ -0,0 +1,17 @@
+package VentueCapitalSystem;
+
+public class Login {
+
+    public static void main(String[] args) {
+
+        System.out.println("this is Login");
+
+        System.out.println("ths is login again");
+    }
+
+
+
+
+
+
+}
